844a3754b17247b52b56594cee73f7a5e521cf83
Dawid Pacanowski <dawidpacanowski1@gmail.com>
2023-11-20 22:06:26 +0100

    feat: login screen design added

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,15 +1,37 @@
 import Image from "next/image";
 import styles from "../styles.module.css";
-import { Button, Input } from "@nextui-org/react";
+import { Button, Divider, Input, Link } from "@nextui-org/react";
 
 export default function Login() {
   return (
     <section className={styles.wrapper}>
-      <Image src={"/logo.svg"} width={80} height={80} alt={"FireRoll logo"} />
-      <h1>Fire Roll</h1>
-      <Input />
-      <Input />
-      <Button />
+      <Image src={"/logo.svg"} width={120} height={120} alt={"FireRoll logo"} />
+      <h1 className="text-5xl">Fire Roll</h1>
+      <div className={styles.action}>
+        <Input
+          radius="sm"
+          type="email"
+          label="Email"
+          variant="bordered"
+          placeholder="Enter your email"
+        />
+        <div className="mb-1">
+          <Input
+            radius="sm"
+            type="password"
+            label="Password"
+            variant="bordered"
+            placeholder="Enter your password"
+          />
+          <Link className="mt-1" size="sm">
+            Forgot password
+          </Link>
+        </div>
+        <Button radius="sm">Login</Button>
+      </div>
+      <p className="text-small">
+        Need to create an account? <Link size="sm">Sign up</Link>
+      </p>
     </section>
   );
 }
