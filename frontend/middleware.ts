f905fb7768d4d3d6ce2539736f9a34cc8835addb
Dawid <Dawid_pacanowski@wp.pl>
2023-11-21 21:38:26 +0100

    feat: redirect logged-in user to dashboard

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,12 +1,20 @@
 import {
   AUTHORIZED_ROUTES,
+  AUTH_ROUTES,
   NOT_AUTH_REDIRECT_ROUTE,
   ROUTES,
 } from "@utils/constants/routes";
 import { NextRequest, NextResponse } from "next/server";
 
+const isAuth = true;
+
 const middleware = async (req: NextRequest, res: NextResponse) => {
-  if (AUTHORIZED_ROUTES.includes(req.nextUrl.pathname as ROUTES)) {
+  if (isAuth && AUTH_ROUTES.includes(req.nextUrl.pathname as ROUTES)) {
+    req.nextUrl.pathname = ROUTES.HOME;
+    return NextResponse.redirect(req.nextUrl);
+  }
+
+  if (!isAuth && AUTHORIZED_ROUTES.includes(req.nextUrl.pathname as ROUTES)) {
     req.nextUrl.pathname = NOT_AUTH_REDIRECT_ROUTE;
     return NextResponse.redirect(req.nextUrl);
   }
