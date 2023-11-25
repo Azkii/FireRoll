d44470270467719ce34af852d568a3de7031d414
Dawid <Dawid_pacanowski@wp.pl>
2023-11-25 20:25:18 +0100

    feat: join room section added

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,5 +1,24 @@
+import { Button, Input } from "@nextui-org/react";
 import React from "react";
 
 export default function HomePage() {
-  return <section>Home page content</section>;
+  return (
+    <div className="flex flex-col items-center">
+      <section className="max-w-2xl w-full flex flex-col text-center gap-4">
+        <h1 className="text-3xl font-bold">Join room</h1>
+        <form className="flex items-center gap-2">
+          <Input
+            size="lg"
+            radius="sm"
+            type="text"
+            placeholder="Room code"
+            labelPlacement="outside"
+          />
+          <Button size="lg" radius="sm">
+            Join
+          </Button>
+        </form>
+      </section>
+    </div>
+  );
 }
