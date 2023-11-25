a88c85979fae22be0f5abf188b99bc4f990a2a27
Dawid <Dawid_pacanowski@wp.pl>
2023-11-25 22:22:35 +0100

    feat: join room form handling added

---
+++ frontend/app/(home)/components/JoinRoomForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+export const JoinRoomForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<{ code: string }>();
+  const onSubmit: SubmitHandler<{ code: string }> = (data) => {
+    console.log(data);
+    setIsSubmitting(true);
+  };
+
+  return (
+    <form className="flex gap-2" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        {...(register("code"), { required: true, minLength: 4 })}
+        size="lg"
+        radius="sm"
+        type="text"
+        placeholder="Room code"
+        labelPlacement="outside"
+        disabled={isSubmitting}
+        errorMessage={
+          <div className="text-left mt-1">{errors?.code?.message}</div>
+        }
+      />
+      <Button size="lg" radius="sm" type="submit" isLoading={isSubmitting}>
+        {isSubmitting ? "" : "Join"}
+      </Button>
+    </form>
+  );
+};
--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,23 +1,12 @@
-import { Button, Input } from "@nextui-org/react";
 import React from "react";
+import { JoinRoomForm } from "./components/JoinRoomForm";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center">
       <section className="max-w-2xl w-full flex flex-col text-center gap-4">
         <h1 className="text-3xl font-bold">Join room</h1>
-        <form className="flex items-center gap-2">
-          <Input
-            size="lg"
-            radius="sm"
-            type="text"
-            placeholder="Room code"
-            labelPlacement="outside"
-          />
-          <Button size="lg" radius="sm">
-            Join
-          </Button>
-        </form>
+        <JoinRoomForm />
       </section>
     </div>
   );
